716805fab53feb5257d5b2c8f3e9f07dcb9803e2
Henry McGovern <henry@smallcloud.it>
2016-05-03 17:41:32 -0400

    precompile assets for heroku

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module Beaulieu
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    #adding asset precompilation per refinery heroku guide
+    config.assets.initialize_on_precompile = true
   end
 end
