77aa3c769420055faa3c0dad7b296b886c95e081
Henry McGovern <henry@smallcloud.it>
2016-05-03 21:21:59 -0400

    add s3 and google analytics

--- config/initializers/refinery/core.rb
+++ config/initializers/refinery/core.rb
@@ -8,10 +8,10 @@ Refinery::Core.configure do |config|
 
   # Set the S3 options using means other than securely by environment variables.
   # If you have to.
-  # config.s3_bucket_name = ENV['S3_BUCKET']
+  config.s3_bucket_name = ENV['beaulieu-prod']
   # config.s3_region = ENV['S3_REGION']
-  # config.s3_access_key_id = ENV['S3_KEY']
-  # config.s3_secret_access_key = ENV['S3_SECRET']
+  config.s3_access_key_id = ENV['AKIAIZ4GTXMGXDJIZTSQ']
+  config.s3_secret_access_key = ENV['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']
 
   # When true will use Amazon's Simple Storage Service instead of
   # the default file system for storing resources and images
@@ -32,7 +32,7 @@ Refinery::Core.configure do |config|
   # This activates Google Analytics tracking within your website. If this
   # config is left blank or set to UA-xxxxxx-x then no remote calls to
   # Google Analytics are made.
-  # config.google_analytics_page_code = "UA-xxxxxx-x"
+  config.google_analytics_page_code = "UA--77277454-1"
 
   # Enable/disable authenticity token on frontend
   # config.authenticity_token_on_frontend = false
